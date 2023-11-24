9c1c6a5dc67a02dd0f39bd5923b14236df990c00
Daniel <uzo1209@gmail.com>
2023-11-24 14:41:26 +0100

    EXTRA - Added more users

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,18 @@ var chiamataInput = document.getElementById("chiamata");
 var chiamataForm = document.getElementById("chiamataForm");
 var creditoResiduo = document.getElementById("creditoresiduo");
 var chiamatePar = document.getElementById("chiamate");
+var ricaricaInput2 = document.getElementById("ricarica2");
+var ricaricaForm2 = document.getElementById("ricaricaForm2");
+var chiamataInput2 = document.getElementById("chiamata2");
+var chiamataForm2 = document.getElementById("chiamataForm2");
+var creditoResiduo2 = document.getElementById("creditoresiduo2");
+var chiamatePar2 = document.getElementById("chiamate2");
+var ricaricaInput3 = document.getElementById("ricarica3");
+var ricaricaForm3 = document.getElementById("ricaricaForm3");
+var chiamataInput3 = document.getElementById("chiamata3");
+var chiamataForm3 = document.getElementById("chiamataForm3");
+var creditoResiduo3 = document.getElementById("creditoresiduo3");
+var chiamatePar3 = document.getElementById("chiamate3");
 var User = /** @class */ (function () {
     function User(_carica, _numeroChiamate) {
         this.carica = _carica;
@@ -14,7 +26,7 @@ var User = /** @class */ (function () {
     };
     User.prototype.chiamata = function (minutiDurata) {
         this.numeroChiamate += 1;
-        return this.carica = this.carica - (minutiDurata * 0.20);
+        return this.carica = parseFloat((this.carica - minutiDurata * 0.20).toFixed(2));
     };
     User.prototype.numero404 = function () {
         return this.carica;
@@ -28,18 +40,8 @@ var User = /** @class */ (function () {
     return User;
 }());
 var cliente1 = new User(0, 0);
-// cliente1.ricarica(50)
-// console.log(cliente1.carica)
-// cliente1.chiamata(10)
-// console.log(cliente1.carica)
-// console.log(cliente1.numeroChiamate)
-// cliente1.chiamata(10)
-// cliente1.chiamata(10)
-// cliente1.chiamata(10)
-// console.log(cliente1.carica)
-// console.log(cliente1.numeroChiamate)
-// cliente1.azzeraChiamate()
-// console.log(cliente1.numeroChiamate)
+var cliente2 = new User(0, 0);
+var cliente3 = new User(0, 0);
 ricaricaForm.addEventListener("submit", function (e) {
     e.preventDefault();
     var importoStr = ricaricaInput.value;
@@ -56,4 +58,38 @@ chiamataForm.addEventListener("submit", function (e) {
     chiamatePar.innerText = cliente1.numeroChiamate.toString();
     creditoResiduo.innerHTML = cliente1.carica.toString();
     chiamataInput.value = '';
+});
+ricaricaForm2.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var importoStr = ricaricaInput2.value;
+    var importo = parseInt(importoStr);
+    cliente2.ricarica(importo);
+    creditoResiduo2.innerText = cliente2.carica.toString();
+    ricaricaInput2.value = '';
+});
+chiamataForm2.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var chiamataStr = chiamataInput2.value;
+    var minuti = parseInt(chiamataStr);
+    cliente2.chiamata(minuti);
+    chiamatePar2.innerText = cliente2.numeroChiamate.toString();
+    creditoResiduo2.innerHTML = cliente2.carica.toString();
+    chiamataInput2.value = '';
+});
+ricaricaForm3.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var importoStr = ricaricaInput3.value;
+    var importo = parseInt(importoStr);
+    cliente3.ricarica(importo);
+    creditoResiduo3.innerText = cliente3.carica.toString();
+    ricaricaInput3.value = '';
+});
+chiamataForm3.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var chiamataStr = chiamataInput3.value;
+    var minuti = parseInt(chiamataStr);
+    cliente3.chiamata(minuti);
+    chiamatePar3.innerText = cliente3.numeroChiamate.toString();
+    creditoResiduo3.innerHTML = cliente3.carica.toString();
+    chiamataInput3.value = '';
 });
--- assets/scripts/script.ts
+++ assets/scripts/script.ts
@@ -4,6 +4,21 @@ const chiamataInput = document.getElementById("chiamata") as HTMLInputElement
 const chiamataForm = document.getElementById("chiamataForm") as HTMLFormElement
 const creditoResiduo = document.getElementById("creditoresiduo") as HTMLParagraphElement
 const chiamatePar = document.getElementById("chiamate") as HTMLParagraphElement
+
+const ricaricaInput2 = document.getElementById("ricarica2") as HTMLInputElement
+const ricaricaForm2 = document.getElementById("ricaricaForm2") as HTMLFormElement
+const chiamataInput2 = document.getElementById("chiamata2") as HTMLInputElement
+const chiamataForm2 = document.getElementById("chiamataForm2") as HTMLFormElement
+const creditoResiduo2 = document.getElementById("creditoresiduo2") as HTMLParagraphElement
+const chiamatePar2 = document.getElementById("chiamate2") as HTMLParagraphElement
+
+const ricaricaInput3 = document.getElementById("ricarica3") as HTMLInputElement
+const ricaricaForm3 = document.getElementById("ricaricaForm3") as HTMLFormElement
+const chiamataInput3 = document.getElementById("chiamata3") as HTMLInputElement
+const chiamataForm3 = document.getElementById("chiamataForm3") as HTMLFormElement
+const creditoResiduo3 = document.getElementById("creditoresiduo3") as HTMLParagraphElement
+const chiamatePar3 = document.getElementById("chiamate3") as HTMLParagraphElement
+
 interface Smartphone {
     carica: number;
     numeroChiamate: number;
@@ -22,9 +37,9 @@ class User implements Smartphone {
     }
     chiamata(minutiDurata: number): number {
         this.numeroChiamate += 1;
-        return this.carica = this.carica - (minutiDurata * 0.20)
+        return this.carica = parseFloat((this.carica - minutiDurata * 0.20).toFixed(2))
     }
-    numero404(): number {
+    numero404() {
         return this.carica
     }
     getNumberChiamate(): number {
@@ -37,18 +52,9 @@ class User implements Smartphone {
 }
 
 let cliente1 = new User(0, 0)
-// cliente1.ricarica(50)
-// console.log(cliente1.carica)
-// cliente1.chiamata(10)
-// console.log(cliente1.carica)
-// console.log(cliente1.numeroChiamate)
-// cliente1.chiamata(10)
-// cliente1.chiamata(10)
-// cliente1.chiamata(10)
-// console.log(cliente1.carica)
-// console.log(cliente1.numeroChiamate)
-// cliente1.azzeraChiamate()
-// console.log(cliente1.numeroChiamate)
+let cliente2 = new User(0, 0)
+let cliente3 = new User(0, 0)
+
 
 ricaricaForm.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -73,4 +79,53 @@ chiamataForm.addEventListener("submit", (e)=> {
     chiamataInput.value = ''
 
 } )
+
+ricaricaForm2.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const importoStr = ricaricaInput2.value;
+    const importo = parseInt(importoStr)
+    cliente2.ricarica(importo)
+    creditoResiduo2.innerText = cliente2.carica.toString()
+    ricaricaInput2.value = ''
+
+    
+})
+
+chiamataForm2.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+    const chiamataStr = chiamataInput2.value;
+    const minuti = parseInt(chiamataStr);
+    cliente2.chiamata(minuti)
+    chiamatePar2.innerText = cliente2.numeroChiamate.toString()
+    creditoResiduo2.innerHTML = cliente2.carica.toString()
+    chiamataInput2.value = ''
+
+} )
+
+ricaricaForm3.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const importoStr = ricaricaInput3.value;
+    const importo = parseInt(importoStr)
+    cliente3.ricarica(importo)
+    creditoResiduo3.innerText = cliente3.carica.toString()
+    ricaricaInput3.value = ''
+
+    
+})
+
+chiamataForm3.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+    const chiamataStr = chiamataInput3.value;
+    const minuti = parseInt(chiamataStr);
+    cliente3.chiamata(minuti)
+    chiamatePar3.innerText = cliente3.numeroChiamate.toString()
+    creditoResiduo3.innerHTML = cliente3.carica.toString()
+    chiamataInput3.value = ''
+
+} )
+
 
